eb52ffb1eb202c14ef209d64b9e07c3a190f2a1a
stephenandersondev <stephenandersondev@gmail.com>
2020-10-30 10:04:34 -0500

    Done.

--- src/components/GalaxySNote7.js
+++ src/components/GalaxySNote7.js
@@ -19,23 +19,31 @@ export default class GalaxySNote7 extends React.Component {
   }
 
   throwAFit = () => {
+    this.setState({
+      panicked: true
+    })
   }
 
   relax = () => {
+    this.setState({
+      panicked: false
+    })
   }
 
-  exclaim = () => {
+  panic = () => {
     if (this.state.panicked) return
+    this.setState({panicked: true})
+    setTimeout(this.relax, 2000)
     this.exclaimAudio.play()
     this.squeelAudio.play()
   }
 
-  panic = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
+  exclaim = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
 
   render() {
-    return(
-      <div id="galaxy-s-note" onClick={this.exclaim}>
-        {(this.state.panicked) ? this.panic() : null}
+    return (
+      <div id="galaxy-s-note" onClick={this.panic}>
+        {(this.state.panicked) ? this.exclaim() : null}
       </div>
     )
   }
